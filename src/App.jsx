d7b62eadfd95cb44d2e8d3e909db18034518cbb0
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2022-10-05 20:48:40 -0700

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg';
 import Banner from './components/Banner'
 import CourseList from './components/CourseList'
