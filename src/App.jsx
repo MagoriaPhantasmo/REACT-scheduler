ccadeecb0377fa8fd1fb827577a55464537a0ecb
MagoriaPhantasmo <donaldbaracskay2022@u.northwestern.edu>
2022-10-06 00:01:35 -0700

    added term selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import Banner from './components/Banner'
-import CourseList from './components/CourseList'
+import TermPage from './components/TermPage'
+
 import './App.css';
+
+
 
 const Main = () => {
   const [schedule, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php")
 
+
   if (error) return <h1>Error loading schedule: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading schedule...</h1>;
   if (!schedule) return <h1>No user data found</h1>;
+ 
  
   return (
     <div>
@@ -21,7 +27,7 @@ const Main = () => {
       </h1>
 
       <div>
-        <CourseList courses={schedule.courses} />
+        <TermPage courses={schedule.courses}/>
       </div>
     </div>
   )
